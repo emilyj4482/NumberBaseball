06157807e0f1953d8d702f0c28b6f27298ba6e28
emilyj4482 <emilyj4482@gmail.com>
2024-11-06 16:58:38 +0900

    [rev] 주석 정리

--- NumberBaseball/Programme.swift
+++ NumberBaseball/Programme.swift
@@ -13,6 +13,10 @@ class Programme {
     private var count: Int = 0          // 단일 게임 guess 횟수가 저장될 빈 값
     private var counts: [Int] = []      // 게임 기록이 저장될 빈 값
     
+    /**
+     프로그램 실행 함수
+     환영 문구를 출력하고, 다음 동작에 대한 사용자의 선택값을 받아 액션을 분기하는 함수에 전달한다.
+     */
     func welcomeMessage() {
         print("Welcome to Number Baseball Game! Select a number.\n[1] Game Start [2] Records [3] Exit")
         guard let input = readLine()
@@ -23,6 +27,11 @@ class Programme {
         separateActions(input)
     }
     
+    /**
+     동작 분기 함수
+     사용자가 입력한 값을 받고, 사용자 선택에 따라 다른 동작을 호출한다.
+     유효하지 않은 값에 대해서는 에러 메시지를 출력하고 다시 `프로그램 실행 함수`로 회귀한다.
+     */
     private func separateActions(_ input: String) {
         switch input {
         case "1":
@@ -37,6 +46,11 @@ class Programme {
         }
     }
     
+    /**
+     기록 조회 함수
+     `counts`에 저장되어 있는 게임 별 count를 출력한다.
+     출력이 완료되면 사용자에게 다음 동작을 묻는 함수를 호출한다.
+     */
     private func getRecords() {
         print("[Your games so far]")
         if counts.count < 1 {
@@ -49,6 +63,9 @@ class Programme {
         askGoingBack()
     }
     
+    /**
+     동작 분기 함수
+     */
     private func askGoingBack() {
         print("Want to go back to main menu?\n[1] Yes. [2] No, exit.")
         guard let input = readLine()
@@ -67,14 +84,16 @@ class Programme {
         }
     }
 
-    // 프로그램 완전 종료 지점
+    /**
+     프로그램 종료 함수
+     */
     private func Exit() {
         print("Bye.")
     }
     
     /**
      게임 실행 함수
-     1. `answer`에 `getAnswer()` 함수를 통해 뽑은 3개의 수를 담은 뒤
+     1. `answer`에 정답 제작하여 할당
      2. `Game Start` 문구 출력
      3. 입력값을 받는 `getInput()` 함수 호출
      */
@@ -86,11 +105,11 @@ class Programme {
     }
 
     /**
-     게임 진행 함수 : 입력값을 받고, 값에 대한 검사를 진행
-     1. 안내 문구 출력
-     2. `input` 값에 대한 유효 검사를 `examInput` 함수를 통해 진행
-     3. `getInput` 함수를 통해 정답 검사 진행 (게임 플레이)
-     4. 정답 시 `Game End` 문구 출력 및 게임 종료
+     게임 진행 함수
+     사용자의 입력값을 받고, 값에 대한 검사를 진행한다.
+     1. `input` 값에 대한 유효 검사를 `examInput` 함수를 통해 진행
+     2. 유효성 검사를 통과한 값에 대해 `examAnswer` 함수를 통해 정답 검사 진행 (게임 플레이)
+     4. 정답 시 게임이 종료되고 다음 동작에 대한 선택지를 받는 함수 호출
      */
     private func getInput() {
         print("Put a 3-digit number down below.")
@@ -119,6 +138,9 @@ class Programme {
         }
     }
     
+    /**
+     동작 분기 함수
+     */
     private func askPlayingAgain() {
         print("Play Again?\n[1] Yes. [2] No, go to main. [3] No, exit.")
         guard let input = readLine()
@@ -141,7 +163,6 @@ class Programme {
 
     /**
      입력값 유효 검사 함수
-     - `input` : `readLine()`을 통해 받은 입력값 String
      - `guard`문을 통해 유효하지 않은 값이 입력될 때마다 에러 메시지를 출력하고 `false`를 return하며 exit
      - 입력값이 유효하여 모든 `guard`문을 통과할 경우 `true`를 return
      */
@@ -171,10 +192,9 @@ class Programme {
 
     /**
      정답 검사 및 힌트 제공 함수
-     - `input` 값이 `[Character]`인 이유는 `String`인 `readLine()` 값에 대해 `.map { $0 }`한 값을 받기 때문이다.
      1) `anwser`과 `input` array를 동시에(`zip`) 순환하며(`forEach`) 자리와 값이 모두 일치하는 경우 `strike`의 count를 증가
-     2) 교집합 메소드인 `intersection`을 사용하기 위해 `Set` 형변환을 하였으며 교집합 개수에서 `strike`를 빼준 값이 `ball`. (자리는 다르지만 같은 값이 존재하는 경우를 count)
-     3) 검사 결과를 포함하는 hint 구문을 출력하는 `printHint` 함수를 호출
+     2) 교집합 메소드인 `intersection`을 사용하기 위해 `Set` 형변환을 하였으며 교집합 개수에서 `strike`를 빼준 값이 `ball`
+     3) 결과에 따라 힌트를 출력하는 `printHint` 함수 호출
      */
     private func examAnswer(_ input: [String]) {
         var strike: Int = 0
@@ -195,7 +215,7 @@ class Programme {
 
     /**
      힌트 출력 함수
-     - `Int` 값인 `strike`, `ball`을 `parameter`로 받아 개수에 따른 메시지를 제작하여 출력
+     - `strike`와 `ball` 개수에 따른 메시지를 제작하여 출력
      - count가 0일 경우 0개라고 알려주는 것이 아니라 아예 출력하지 않도록 처리
      - 둘다 0일 경우 out 출력
      */
